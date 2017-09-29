377f390e22deacdfaed1adb7238d569d012ec71e
Jon_Nieblas <jniebla1@msudenver.edu>
2017-09-28 23:52:25 -0600

    Make a basic User model (including secure passwords)

--- generate/db/schema.rb
+++ generate/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928162322) do
+ActiveRecord::Schema.define(version: 20170929053242) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20170928162322) do
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_sections_on_course_id"
     t.index ["professor_id"], name: "index_sections_on_professor_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
