808d3a92f30e61b69236fd4ee3868b933b80b05e
Jon_Nieblas <jniebla1@msudenver.edu>
2017-10-12 11:54:57 -0600

    Views for student and professor now align with Assignment 3 requirements. Fixed url bug with the drop down menu. Adjusted view for enrollments. All requirements have officially been met for assignment 3.

--- generate/app/models/enrollment.rb
+++ generate/app/models/enrollment.rb
@@ -1,4 +1,6 @@
 class Enrollment < ApplicationRecord
   belongs_to  :section
   belongs_to  :student
+
+  validates :enroll, uniqueness: { scope: [:section_id, :student_id]}
 end
--- generate/app/models/student.rb
+++ generate/app/models/student.rb
@@ -3,6 +3,6 @@ class Student < ApplicationRecord
   has_many :sections, through: :enrollments
   has_many :professors, through: :sections
 
-  validates :name, length: { maximum: 20 }
+  validates :name, length: { in: 5..20}
   validates :name, uniqueness: true
 end
