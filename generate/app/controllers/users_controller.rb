3a6f062ae6825cc38a1f2f7b7ab99199a0568088
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-03-27 16:23:44 -0600

    fixed some dependencies. Also pushing tests.

--- generate/app/controllers/users_controller.rb
+++ generate/app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Registration App!"
-      redirect_to @course
+      redirect_to @user
     else
       render 'new'
     end
